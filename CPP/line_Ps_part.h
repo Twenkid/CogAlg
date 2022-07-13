54a0eca8114dac37fd30fbc0b27d8f46254982c9
Todor Arnaudov <twenkid@gmail.com>
2022-07-13 04:27:38 +0300

    Fixed bugs, timing: min ~ 0.292 s, i5-6500
    A few misses in the first "hack" - I've missed to create the new dert_ instances.
    Min time ~ 0.292 s.

--- CPP/line_Ps_part.h
+++ CPP/line_Ps_part.h
@@ -1,29 +1,41 @@
 #pragma once
 
-#include <algorithm>
-#include <array>
 #include <vector>
-#include <math.h>
 #include "core.hpp"
 #include "opencv2/core/utility.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
-
 #include "core\core.hpp"
+#include "../ConsoleApplication1/Utils/VariousUtils.h"  //timing
 
-/* 12.July.2022 
- C++ Port of Alex Pitertsev's port to Julia of Boris Kazachenko et al. Python level 1D algorithm from CogAlg:
+//#include <algorithm>
+//#include <array>
+//#include <math.h>
+
+/* 12.July.2022, 13.7.2022
+ C++ Port by Twenkid of Alex Pitertsev's port to Julia of Boris Kazachenko et al. Python level 1D algorithm from CogAlg:
  https://github.com/alex-pitertsev/CogAlg/blob/4ce0256d6a20a571d0e7243cdb0042d7037eee5d/line_1D_alg/julia/line_Ps_part.jl
- C++ version by Twenkid - this is just a first quick draft done for 2 hours which is to show to the team how it looks like etc.
- PmPd are empty, why?
+ 
+ This is just a first quick draft done for 2 hours and a bit which is to show to the team how it looks like etc.
+ PmPd are empty, why? - fixed, I've missed to add the creation of the new dert_;
+ A few additional checks had to be added due to the lack of "nothing" in C++ in order to avoid attempt to acceess an empty P_ or something
+ 
+ Tests with several tweaks with references and sending only the row of the Mat instead of slicing, but it seems the difference may be rather from different background loading.
+ Best time of the main cycle traversing 768 lines of the raccoon.jpg is about 0.292 s, it varies to up to 0.3xx. 
+
 */ 
 
 namespace Line1D {
+    SYSTEMTIME mStartTime, mEndTime;
+    CTiming timing; //Utils/VariousUtils.h    
+   
 
     typedef cv::Point_<uint8_t> Pixel;
 
     /*
+    CogAlg comments:
+
     line_Ps is a principal version of 1st - level 1D algorithm
     Operations :
     -
@@ -43,7 +55,8 @@ namespace Line1D {
     These forks here are exclusive per P to avoid redundancy, but they do overlap in line_patterns_olp.
     """
     */
-    //Conversion from Alex code to C++, Todor, #12-7-2022
+
+    //Conversion from Alex code to C++, Todor, #12-7-2022, bug fixes and timing: 13-7-2022
 
     //using Images, ImageView, CSV
 
@@ -59,10 +72,10 @@ namespace Line1D {
         }
     };
 
-    //dert_ = Cdert_[] # line - wide i_, p_, d_, m_, mrdn_
+    //dert_ = Cdert_[] # line - wide i_, p_, d_, m_, mrdn_  --> local
 
     //std::array<Cdert_, 3> dert_;
-    std::vector<Cdert_> dert_;
+   // std::vector<Cdert_> dert_;
 
     struct CP {
         int L, I, D, M, Rdn, x0;
@@ -106,23 +119,25 @@ namespace Line1D {
     */
     bool logging = true;
 
+    //std::vector<CP> form_P_(void* rootP, std::vector<Cdert_> dert_, int rdn, int rng, int fPd)
     std::vector<CP> form_P_(void* rootP, std::vector<Cdert_> dert_, int rdn, int rng, int fPd)
     {  // rdn, rng, fPd) {
         //function form_P_(rootP, dert_; rdn, rng, fPd)  # after semicolon in Julia keyword args are placed
         //# initialization:
           //P_ = CP[]  # structure to store all the form_P(layer1) output
+        
         auto P_ = std::vector<CP>(); // CP[]  # structure to store all the form_P(layer1) output
         int x = 0;
         bool _sign=false, sign=!_sign; // = nothing  # to initialize 1st P, (nothing != True) and (nothing != False) are both True
+        bool b_first_run = true;
 
-        for (auto dert : dert_) {
+        for (auto const dert : dert_) {
             //# segment by sign
             if (fPd) sign = dert.d > 0;
             else
                 sign = dert.m > 0;
 
-
-            if (sign != _sign) {
+            if (sign != _sign || b_first_run) { //first are set to be diff
                 //# sign change, initializeand append P
                 auto L = 1;
                 auto I = dert.p;
@@ -134,17 +149,31 @@ namespace Line1D {
                 auto new_dert_ = std::vector<Cdert_>({ dert });
                 auto cp = CP(L, I, D, M, Rdn, x0, new_dert_); // std::vector<Cdert_>({ dert }
                 P_.push_back(cp);
+                b_first_run = false; 
             }
             else
-            {//# accumulate params :        
-                auto end = P_.back();
-                end.L += 1;
-                end.I += dert.p;
-                end.D += dert.d;
-                end.M += dert.m;
-                end.Rdn += dert.mrdn;
-                //push!(P_[end].dert_, dert)
-                end.dert_.push_back(dert);
+            {//# accumulate params :  
+                //if empty - error when trying to access P_.back()! it shoudn't be empty?
+                if (P_.size() == 0) {
+                    auto end = P_.front(); // .back();
+                    end.L += 1;
+                    end.I += dert.p;
+                    end.D += dert.d;
+                    end.M += dert.m;
+                    end.Rdn += dert.mrdn;
+                    //push!(P_[end].dert_, dert)
+                    end.dert_.push_back(dert);
+                }
+                else {
+                    auto end = P_.back();
+                    end.L += 1;
+                    end.I += dert.p;
+                    end.D += dert.d;
+                    end.M += dert.m;
+                    end.Rdn += dert.mrdn;
+                    //push!(P_[end].dert_, dert)
+                    end.dert_.push_back(dert);
+                }
             }
             x += 1;
             _sign = sign;
@@ -153,38 +182,115 @@ namespace Line1D {
         if (logging == 1)
             if (!fPd) {
                 //CSV.write("./layer1_Pm_log_jl.csv", P_)
-                printf("log layer1_Pm_log....");
+                //printf("log layer1_Pm_log....");
             }
             else
             {
                 //CSV.write("./layer1_Pd_log_jl.csv", P_)
-                printf("Pd_log... {_");
+                //printf("Pd_log... {_");
             }
         return P_; // # used only if not rootP, else packed in rootP.sublayers
     }
 
-    std::vector<std::vector<CP>> line_Ps_root(cv::Mat pixel_) {
-        printf("line_Ps_root");
+   //returning a tuple (pair) instead of a vector, #13-7-2022
+    //std::pair <std::vector<CP>, std::vector<CP>> line_Ps_root_pair(cv::Mat pixel_) {
+    std::pair <std::vector<CP>, std::vector<CP>> line_Ps_root_pair(cv::Mat pixel_) { //only one line is sent
+        //printf("line_Ps_root");
+        std::vector<Cdert_> dert_; // //local  # line - wide i_, p_, d_, m_, mrdn_
         //auto_i = frame.at<Pixel>(r, c) = pixel;  //pixel_[0];
-        auto _i = pixel_.at<Pixel>(0, 0); // = pixel;  //pixel_[0];    
+        auto _i = pixel_.at<Pixel>(0, 0).x; // = pixel;  //pixel_[0];    
         //auto shift = pixel_.colRange(1, pixel_.cols);
 
         for (auto c = 1; c < pixel_.cols; c++) {
-            auto i = pixel_.at<Pixel>(0, c) = pixel_.at<Pixel>(0, c);  //pixel;
-            auto d = i.x - _i.x;
-            auto p = i.x + _i.x;
-            auto m = ave - abs(d);
-            auto mrdn = m < 0;
+            int i = pixel_.at<Pixel>(0, c).x; // = pixel_.at<Pixel>(0, c);  //pixel;
+            int d = i - _i;
+            int p = i + _i;
+            int m = ave - abs(d);
+            int mrdn = m < 0;
+            //push!(dert_, Cdert_(i, p, d, m, mrdn))  # save data in the struct
+            //_i = i      
+            //auto new_dert_ = std::vector<Cdert_>({ dert });
+            //auto cp = CP(L, I, D, M, Rdn, x0, new_dert_);
+            dert_.push_back(Cdert_(i, p, d, m, mrdn));
         }
         //# form patterns, evaluate them for rng + and der + sub - recursion of cross_comp :
         auto Pm_ = form_P_(NULL, dert_, 1, 1, false); // rdn = 1, rng = 1, fPd = false)  # rootP = None, eval intra_P_(calls form_P_)
         auto Pd_ = form_P_(NULL, dert_, 1, 1, true); // rdn = 1, rng = 1, fPd = true); ==> to the global dert_
 
         if (logging == 1) {
-            printf("CSV.write(. / layer0_log_jl.csv, dert_)");
+            //printf("CSV.write(. / layer0_log_jl.csv, dert_)");
             //CSV.write("./layer0_log_jl.csv", dert_)
         }
-        std::vector<std::vector<CP>> PmPd = std::vector<std::vector<CP>>({ Pm_, Pd_ });
+        //std::vector<std::vector<CP>> PmPd = std::vector<std::vector<CP>>({ Pm_, Pd_ });
+        std::pair <std::vector<CP>, std::vector<CP>> PmPd(Pm_, Pd_);
+        return PmPd;
+    }
+
+
+    //returning a tuple (pair) instead of a vector, #13-7-2022
+     //std::pair <std::vector<CP>, std::vector<CP>> line_Ps_root_pair(cv::Mat pixel_) {
+    std::pair <std::vector<CP>, std::vector<CP>> line_Ps_root_pair_rowonly(cv::Mat& pixel_, int y) {
+        //printf("line_Ps_root");
+        std::vector<Cdert_> dert_; // //local  # line - wide i_, p_, d_, m_, mrdn_
+        //auto_i = frame.at<Pixel>(r, c) = pixel;  //pixel_[0];
+        auto _i = pixel_.at<Pixel>(y, 0).x; // = pixel;  //pixel_[0];    
+        //auto shift = pixel_.colRange(1, pixel_.cols);
+
+        for (auto c = 1; c < pixel_.cols; c++) {
+            int i = pixel_.at<Pixel>(y, c).x; // = pixel_.at<Pixel>(0, c);  //pixel;
+            int d = i - _i;
+            int p = i + _i;
+            int m = ave - abs(d);
+            int mrdn = m < 0;
+            //push!(dert_, Cdert_(i, p, d, m, mrdn))  # save data in the struct
+            //_i = i      
+            //auto new_dert_ = std::vector<Cdert_>({ dert });
+            //auto cp = CP(L, I, D, M, Rdn, x0, new_dert_);
+            dert_.push_back(Cdert_(i, p, d, m, mrdn));
+        }
+        //# form patterns, evaluate them for rng + and der + sub - recursion of cross_comp :
+        auto Pm_ = form_P_(NULL, dert_, 1, 1, false); // rdn = 1, rng = 1, fPd = false)  # rootP = None, eval intra_P_(calls form_P_)
+        auto Pd_ = form_P_(NULL, dert_, 1, 1, true); // rdn = 1, rng = 1, fPd = true); ==> to the global dert_
+
+        if (logging == 1) {
+            //printf("CSV.write(. / layer0_log_jl.csv, dert_)");
+            //CSV.write("./layer0_log_jl.csv", dert_)
+        }
+        //std::vector<std::vector<CP>> PmPd = std::vector<std::vector<CP>>({ Pm_, Pd_ });
+        std::pair <std::vector<CP>, std::vector<CP>> PmPd(Pm_, Pd_);
+        return PmPd;
+    }
+
+
+    //std::vector<std::vector<CP>> line_Ps_root(cv::Mat pixel_) {
+    std::vector<std::vector<CP>> line_Ps_root_vector(cv::Mat pixel_) {
+        //printf("line_Ps_root");
+        std::vector<Cdert_> dert_; // //local  # line - wide i_, p_, d_, m_, mrdn_
+        //auto_i = frame.at<Pixel>(r, c) = pixel;  //pixel_[0];
+        auto _i = pixel_.at<Pixel>(0, 0).x; // = pixel;  //pixel_[0];    
+        //auto shift = pixel_.colRange(1, pixel_.cols);
+
+        for (auto c = 1; c < pixel_.cols; c++) {
+            int i = pixel_.at<Pixel>(0, c).x; // = pixel_.at<Pixel>(0, c);  //pixel;
+            int d = i - _i;
+            int p = i + _i;
+            int m = ave - abs(d);
+            int mrdn = m < 0;
+            //push!(dert_, Cdert_(i, p, d, m, mrdn))  # save data in the struct
+            //_i = i      
+            //auto new_dert_ = std::vector<Cdert_>({ dert });
+            //auto cp = CP(L, I, D, M, Rdn, x0, new_dert_);
+            dert_.push_back(Cdert_(i, p, d, m, mrdn));
+        }
+        //# form patterns, evaluate them for rng + and der + sub - recursion of cross_comp :
+        auto Pm_ = form_P_(NULL, dert_, 1, 1, false); // rdn = 1, rng = 1, fPd = false)  # rootP = None, eval intra_P_(calls form_P_)
+        auto Pd_ = form_P_(NULL, dert_, 1, 1, true); // rdn = 1, rng = 1, fPd = true); ==> to the global dert_
+
+        if (logging == 1) {
+            //printf("CSV.write(. / layer0_log_jl.csv, dert_)");
+            //CSV.write("./layer0_log_jl.csv", dert_)
+        }
+        std::vector<std::vector<CP>> PmPd = std::vector<std::vector<CP>>({ Pm_, Pd_ });        
         return PmPd;
         //return[Pm_, Pd_] // # input to 2nd level
     }
@@ -207,7 +313,7 @@ namespace Line1D {
 
     if logging == 1
     CSV.write("./layer0_log_jl.csv", dert_)
-    end
+     end
 
     return[Pm_, Pd_]  # input to 2nd level
     end
@@ -250,21 +356,56 @@ namespace Line1D {
             //frame = []
 
          // # y is index of new row pixel_, we only need one row, use init_y = 1, halt_y = Y for full frame
-        init_y = 500;
+        //init_y = 500;
+        init_y = 0;
+        halt_y = 1000;
 
+        //unsigned y_end = halt_y < Y ? halt_y : Y; //why sets it to 1000, the bigger value?
         unsigned y_end = halt_y < Y ? halt_y : Y;
-        
+
+        cout << "halt_y=" << y_end;
+        printf("\nTIMING for %d, %d", init_y, y_end); // halt_y);
+        mStartTime = timing.now();        
         //for (int y = init_y; y < min(halt_y, Y); y++) {
-        for (unsigned y = init_y; y <= y_end; y++) {
-            printf("\ny=%u, y_end=%u", y, y_end);
+        for (unsigned y = init_y; y < y_end; y++) {
+            //printf("\ny=%u, y_end=%u", y, y_end);
             //line_Ps_root(gray_image_int[y, :])  # line = [Pm_, Pd_]
             //line_Ps_root(gray_image_int[y, :])  # line = [Pm_, Pd_]        
             //NO, col: x, row: y line_Ps_root(image.colRange(y, y+1).rowRange(0, X)); // gray_image_int[y, :])  # line = [Pm_, Pd_]
-            line_Ps_root(image.rowRange(y, y+1).colRange(0, X)); // gray_image_int[y, :])  # line = [Pm_, Pd_]
+            
+            //line_Ps_root_vector(image.rowRange(y, y+1).colRange(0, X)); // gray_image_int[y, :])  # line = [Pm_, Pd_]          
+             line_Ps_root_pair(image.rowRange(y, y+1).colRange(0, X)); // gray_image_int[y, :])  # line = [Pm_, Pd_]
+            //line_Ps_root_pair_rowonly(image, y); // .rowRange(y, y + 1).colRange(0, X)); //0.331, 0.330, 0.328. 0:0:0.335
+            //Similar time, 0.309, 0.327, 0.360, 0.344, 0.362; line 0:0:0.332, 0:0:0.326, 0:0:0.365, 0:0:0.373
+            //turning off some programs, pair: 0:0:0.293, 0:0:0.303, 0:0:0.307, 0:0:0.302s
             //y,y or y,y+1?
+            //System Idle process is loading my computer, timing seems to vary more depending on some background load on my PC, rather than the tweaks with references,
+            //Sending the original image Mat and the row only etc.
+            //Min 0.292, Average about 0.3x s, depending on the system processes load on i5-6500 Win10            
             // Channel_16SC1(Range(0, height), Range(1, width)).copyTo(maskX(Range(0, height), Range(0, width - 1))); //X
         }
+        mEndTime = timing.now();
+        cout << endl << "line_Ps_root_pair" << endl;
+        timing.timeDiff(mStartTime, mEndTime, 1); //print time       
 
+        mStartTime = timing.now();
+        //for (int y = init_y; y < min(halt_y, Y); y++) {
+        for (unsigned y = init_y; y < y_end; y++) {
+            
+            line_Ps_root_vector(image.rowRange(y, y+1).colRange(0, X)); // gray_image_int[y, :])  # line = [Pm_, Pd_]                      
+        }
+        mEndTime = timing.now();
+        cout << endl << "line_Ps_root_vector" << endl;
+        timing.timeDiff(mStartTime, mEndTime, 1); //print time       
+
+        mStartTime = timing.now();
+        //for (int y = init_y; y < min(halt_y, Y); y++) {
+        for (unsigned y = init_y; y < y_end; y++) {            
+           line_Ps_root_pair_rowonly(image, y); // .rowRange(y, y + 1).colRange(0, X)); //0.331, 0.330, 0.328. 0:0:0.335
+        }
+        cout << endl << "line_Ps_root_pair_rowonly" << endl;
+        mEndTime = timing.now();
+        timing.timeDiff(mStartTime, mEndTime, 1); //print time               
     }
 
 }
